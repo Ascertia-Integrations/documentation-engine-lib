db38bbaf73bf938b107eda35223bcc0220d9b84b
Jorge Frias <jorge.frias@ascertia.com>
2026-04-22 17:23:05 +0200

    Centralized Logic in Preset: I updated the docusaurus-preset-docs theme plugin to provide a default, safe themeConfig..

--- packages/docusaurus-preset-docs/src/themePlugin.ts
+++ packages/docusaurus-preset-docs/src/themePlugin.ts
@@ -10,6 +10,42 @@ export default function themePlugin(_context: LoadContext): Plugin<void> {
     getTypeScriptThemePath() {
       return path.resolve(__dirname, "..", "theme");
     },
+    getThemeConfig() {
+      return {
+        navbar: {
+          items: [
+            {
+              type: "docsVersionDropdown",
+              position: "right",
+            },
+          ],
+        },
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Docs",
+              items: [
+                {
+                  label: "Documentation Index",
+                  to: "/",
+                },
+              ],
+            },
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "GitHub",
+                  href: "https://github.com/Ascertia-Integrations",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Ascertia Integrations. Built with Docusaurus.`,
+        },
+      };
+    },
   };
 }
 
